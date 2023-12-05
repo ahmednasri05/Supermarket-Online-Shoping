1d2d109c5e2e914c4ef51440032545d1d3d42823
Hadyaziz05 <hadyabdelaziz05@gmail.com>
2023-12-05 07:35:11 +0200

    integrated opendatabase function

--- OpenDataBase.h
+++ OpenDataBase.h
@@ -11,11 +11,17 @@ sqlite3* openDataBase(string dbFileName)
 {
 	sqlite3* db;
 	sqlite3_open(dbFileName.c_str(), &db);
-	int req = sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, adress TEXT)", NULL, NULL, NULL);
-	checkForError(db, req);
+	int req = sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, password TEXT, phone TEXT, location TEXT)", NULL, NULL, NULL);
+    checkForError(db, req);
 	if (req == 1)
 	{
-		cout << "Table does not exist, thus a new table was created." << endl;
+		cout << "users table does not exist, thus a new table was created." << endl;
+	}
+	int reqProducts = sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, price INTEGER, code TEXT, category TEXT, production_date TEXT, expiration_date TEXT)", NULL, NULL, NULL);
+    checkForError(db, reqProducts);
+	if (req == 1)
+	{
+		cout << "products table does not exist, thus a new table was created." << endl;
 	}
 	cout << "Database " + dbFileName + " opened successfully" << endl;
 	return db;
--- main.cpp
+++ main.cpp
@@ -12,18 +12,11 @@
 
 using namespace std;
 
-sqlite3* createConnection() {
-    sqlite3* db;
-    sqlite3_open("users1.db", &db);
-    int req = sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, password TEXT, phone TEXT, location TEXT)", NULL, NULL, NULL);
-    checkForError(db, req);
-    cout << "Database opened successfully" << endl;
-    return db;
-}
+
 
 int main() {
     sqlite3* db;
-    db = createConnection();
+    db = openDataBase("users1.db");
     Greeting(db);
     return 0;
 }
